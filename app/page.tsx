e06e8b6b09bcbac603f4af860b7a9d303a7decf8
김태헌 <7w7w7w7w@naver.com>
2025-11-27 01:44:10 +0900

    design: 랜딩 페이지 UI 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { LANDING_PAGE } from "@/config/ui-text";
 
 const LandingPage = () => {
   return (
@@ -7,39 +9,64 @@ const LandingPage = () => {
       <header className="px-6 -mt-4 md:-mt-6 lg:-mt-8">
         <Image
           src="/desker-logo.png"
-          width={150}
-          height={150}
+          width={LANDING_PAGE.LOGO_SIZE}
+          height={LANDING_PAGE.LOGO_SIZE}
           alt="Desker Logo"
           priority
         />
       </header>
       <main>
-        <div className="flex justify-center items-center -mt-2 md:-mt-3 lg:-mt-4">
+        <div className="flex justify-center items-center -mt-2 md:-mt-5 lg:-mt-4">
           <h1 className="block font-bold text-4xl text-center">
-            고객과 대화하는 데스커 AI 로<br />
-            떠나는 고객을 붙잡아 매출로 전환하세요
+            {LANDING_PAGE.TITLE_LINE1}
+            <br />
+            {LANDING_PAGE.TITLE_LINE2}
           </h1>
         </div>
-        <div className="pb-10"></div>
-        <div className="flex justify-center items-center gap-10 pb-10">
-          <Link
-            href="/login"
-            className="text-aline gap-5 self-start rounded-lg bg-brown-300 px-7 py-3.5 text-base md:px-8 md:py-4 md:text-lg lg:px-10 lg:py-5 lg:text-xl font-medium text-black transition-colors hover:bg-brown-900 hover:text-white"
+        <div className="pb-10" />
+        <div className="flex justify-center items-center gap-8 md:gap-12 pb-12 mt-3">
+          <Button
+            asChild
+            className="
+              bg-white
+              text-blue-800
+              border border-blue-600
+              hover:bg-blue-600
+              hover:text-white
+              text-xl md:text-xl
+              px-10 md:px-14
+              py-5 md:py-6
+              rounded-xl
+              transition-colors
+            "
           >
-            <span>로그인 하기</span>
-          </Link>
-          <Link
-            href="/login"
-            className="text-aline gap-5 self-start rounded-lg bg-brown-700 px-7 py-3.5 text-base md:px-8 md:py-4 md:text-lg lg:px-10 lg:py-5 lg:text-xl font-medium text-white transition-colors hover:bg-brown-200 hover:text-black"
+            <Link href="/login">{LANDING_PAGE.LOGIN_BUTTUN}</Link>
+          </Button>
+
+          <Button
+            asChild
+            variant="secondary"
+            className="
+              bg-white
+              text-blue-800
+              border border-blue-600
+              hover:bg-blue-600
+              hover:text-white
+              text-xl md:text-xl
+              px-10 md:px-14
+              py-5 md:py-6
+              rounded-xl
+              transition-colors
+            "
           >
-            <span>무료로 체험하기</span>
-          </Link>
+            <Link href="/signup">{LANDING_PAGE.FREE_BUTTUN}</Link>
+          </Button>
         </div>
-        <section className="flex justify-center items-center rounded-lg bg-brown-400 p-0.5 ">
+        <section className="flex justify-center items-center rounded-lg bg-blue-200 p-1 mt-1">
           <Image
             src="/information-section-image.png"
-            width={1280}
-            height={853}
+            width={LANDING_PAGE.IMAGE_SIZE_WIDTH}
+            height={LANDING_PAGE.IMAGE_SIZE_HEIGHT}
             alt="Landing Information Section Image"
             priority
           />
